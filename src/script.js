3ec2b3ed16b8657cde9e32a343647dfb41f8a18c
TiJun <9tomek7@gmail.com>
2024-02-28 00:43:16 +0100

    added scroll padding

--- src/script.js
+++ src/script.js
@@ -1,10 +1,14 @@
 const burger = document.querySelector("#burger");
 const mobileNav = document.querySelector("#mobile-nav");
 const links = document.querySelectorAll("#link");
+const desktopNav = document.querySelectorAll("#desktop-nav");
 const handleNav = () => {
 	mobileNav.classList.toggle("translate-x-[-100%]");
 	mobileNav.classList.toggle("translate-x-0");
 };
+// const scrollSpy = () => {
+//     window.scrollY >=
+// }
 links.forEach(el => {
 	el.addEventListener("click", () => {
 		mobileNav.classList.toggle("translate-x-[-100%]");
